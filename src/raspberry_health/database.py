5ca1256272f227f3c10bac739ebf7bd47e76fc81
alekseyka <aleksey.isk@gmail.com>
2026-09-13 15:58:12 +0300

    Added mapped classes

---
+++ src/raspberry_health/database.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from datetime import datetime
+
+class Base(DeclarativeBase):
+    pass
+
+class Measurement(Base):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    temperature: Mapped[float]
+    source: Mapped[str]
+    date: Mapped[datetime]
